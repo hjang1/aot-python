2cc6b2f888f5e09ca7c42c7fa4446a3e63b8d712
Donghee Kim <nts_dhkim@ntels.com>
2017-03-03 15:42:38 +0900

    updated at 20170303

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='aot-python',
+    version='1.0',
+    packages=['mqtt', 'util', 'examples'],
+    url='www.allofthings.com',
+    license='',
+    author='dhkim',
+    author_email='nts_dhkim@ntels.com',
+    description=''
+)
